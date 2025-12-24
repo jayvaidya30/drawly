1f518c17c82825e418966db0e58d7cb8ee99be7f
Jay <jayvaidya3009@gmail.com>
2025-12-24 13:02:24 +0530

    Add auth to websocket

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,30 @@
 import { WebSocketServer } from "ws";
+const JWT_SECRET = "123";
+import jwt from "jsonwebtoken";
+const wss = new WebSocketServer({ port: 8080 });
 
-const wss = new WebSocketServer({port: 8080})
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
 
-wss.on("connection", function connection(ws){
-    ws.on("message", function(data){
-        ws.send("pong")
-    })
-})
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (typeof decoded == "string") {
+    ws.close();
+    return;
+  }
+
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
+
+  ws.on("message", function (data) {
+    ws.send("pong");
+  });
+});

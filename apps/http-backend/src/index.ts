503d7366d791b32deb59efb5286b7ee712fcc218
Jay <jayvaidya3009@gmail.com>
2025-12-24 12:39:15 +0530

    Add middleware

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import jwt from "jsonwebtoken"
+
+const app = express();
+
+app.post("/signup", async (req, res) => {});
+
+app.post("/signin", async (req, res) => {});
+
+app.post("/room", async (req, res) => {});
+
+app.listen(3001);
